5d31c3a212badfa5c1cf75dacb74141d8d9324d8
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-27 15:41:36 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from "./signup";
 
 describe("Signup controller", () => {
+    
     test("Should return 400 if no name is provided", () => {
         const sut = new SignUpController();
         const httpRequest = {
@@ -16,4 +17,22 @@ describe("Signup controller", () => {
         expect(httpResponse.body).toEqual(new Error("Missing param: name"));
 
     })
+
+    test("Should return 400 if no email is provided", () => {
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: "kellton",
+                password: "123456",
+                passwordConfirmation: "123456",
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+
+    })
+
+
 })
